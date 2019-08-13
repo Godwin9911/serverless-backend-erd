b6f0aacd90722066ab6b233a3a9c8a13249e035b
Godwin9911 <agedah99@gmail.com>
2019-08-13 12:57:01 +0100

    update for local

--- express/app.js
+++ express/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const router = express.Router();
---
+++ server.js
@@ -0,0 +1,3 @@
+const app = require('./express/app');
+
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
